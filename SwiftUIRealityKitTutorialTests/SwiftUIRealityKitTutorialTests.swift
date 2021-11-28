8a91a52fd45fbda67a8236e7e9aafbcea9a43ef8
Elijah Allen <elijah.a.allen@gmail.com>
2021-11-27 18:26:10 -0700

    Initial Commit

---
+++ SwiftUIRealityKitTutorialTests/SwiftUIRealityKitTutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIRealityKitTutorialTests.swift
+//  SwiftUIRealityKitTutorialTests
+//
+//  Created by Gove Allen on 11/27/21.
+//
+
+import XCTest
+@testable import SwiftUIRealityKitTutorial
+
+class SwiftUIRealityKitTutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
